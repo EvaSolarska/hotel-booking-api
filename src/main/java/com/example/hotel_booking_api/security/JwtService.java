66190229a62dd24509dfbed95bbf1dbb882c3eee
Ewa <116517774+EvaSolarska@users.noreply.github.com>
2026-05-16 18:29:04 +0200

    fix: handle invalid and expired JWT tokens

--- src/main/java/com/example/hotel_booking_api/security/JwtService.java
+++ src/main/java/com/example/hotel_booking_api/security/JwtService.java
@@ -1,8 +1,6 @@
 package com.example.hotel_booking_api.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,8 +39,12 @@ public class JwtService {
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        String email = extractEmail(token);
-        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        try {
+            String email = extractEmail(token);
+            return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        } catch (JwtException e) {
+            return false;
+        }
     }
 
     private boolean isTokenExpired(String token) {
